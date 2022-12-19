f2821808e0f19b080adde06222cb0cab41428a6a
Igor <fdiii@mail.ru>
2022-12-20 00:52:12 +0300

    Add tests and GH Actions

---
+++ src/FuncHW/Functions/PersonFunctions.cs
@@ -0,0 +1,13 @@
+namespace FuncHW.Functions
+{
+    public static class PersonFunctions
+    {
+        //public static Func*** FuncPersonIsActive = ***;
+
+        //public static Func*** FuncPersonPhoneNumberStartsWith7 = ***;
+
+        //public static Func*** FuncPersonIsChild = ***;
+
+        //public static Func*** FuncPersonHasShortName = ***;
+    }
+}
